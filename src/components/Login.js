6138334355cd3fb7af789254b89d1ddbd580af99
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-09-19 22:15:15 +0300

    feat: add authorization fetch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import * as auth from '../mestoAuth';
 
 function Login() {
 
@@ -7,14 +8,24 @@ function Login() {
   const handleChangeInputs = (evt) => {
     const newLoginData = { [evt.target.type]: evt.target.value };
     setLoginData((userData) => ({ ...loginData, ...newLoginData }));
+  }
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const {email, password} = loginData;
+
+    auth.authorization(email, password)
+      .then(data => console.log(data));
+      //сохранить токен в локалсторэдж
+      //Изменить стейт loggedIn
+      //Перенаправить на /
   }
 
   
   return (
     <div className="login">
       <h2 className="login__title">Вход</h2>
-      <form action="#" className="login__form">
+      <form action="#" className="login__form" onSubmit={handleSubmit}>
         <fieldset className="login__fieldset">
           <input type="email" className="login__input" placeholder="Email" value={loginData.email} onChange={handleChangeInputs} />
           <input type="password" className="login__input" placeholder="Пароль" value={loginData.password} onChange={handleChangeInputs} />
