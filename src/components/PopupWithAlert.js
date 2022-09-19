e71cc86cc3cf7fd685f92158b97fe102d1ce83a9
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-09-19 12:08:12 +0300

    fix: align for title in alert popup

--- src/components/PopupWithAlert.js
+++ src/components/PopupWithAlert.js
@@ -6,7 +6,7 @@ function PopupWithAlert() {
     <div className="popup popup_type_alert popup_opened">
       <div className="popup__container">
         <img src={successIcon} alt="" className="popup__icon" />
-        <h2 className="popup__title">Вы успешно зарегистрировались!</h2>
+        <h2 className="popup__title popup__title_align_center">Вы успешно зарегистрировались!</h2>
         <button
           className="popup__close"
           type="button"
